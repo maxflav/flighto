fc6acfc8a03e34eb8b247d5a3a35f8aff76e1bce
Max Friedman <maxcfriedman@gmail.com>
2018-09-11 14:13:05 -0700

    one_trip

--- flighto.py
+++ flighto.py
@@ -1,12 +1,7 @@
 import requests
+from pprint import pprint
 
-url = 'https://www.hipmunk.com/api/flights/v3/load_search_first'
-
-payload = {
-    'date0': '11/22/2018',
-    'from0': 'LHR',
-    'to0': 'JFK',
-}
+first_url = 'https://www.hipmunk.com/api/flights/v3/load_search_first'
 
 headers = {
     'accept':'application/json, text/javascript, */*; q=0.01',
@@ -23,32 +18,91 @@ headers = {
     'x-requested-with':'XMLHttpRequest',
 }
 
-try:
-    r = requests.post(url, headers=headers, data=payload)
-except:
-    print('failed to request', sys.exc_info()[0])
+stopovers = ['LHR', 'LGW', 'BOS', 'JFK']
 
-print('done requesting')
+# filtered trips where price or time is > 2.5 the minimum
+# {
+#   layovers: [{airport: "CODE", layover: seconds}],
+#   time: seconds,
+#   depart: iso in local time,
+#   arrive: iso in local time,
+#   price: USD,
+#   airlines: [code],
+#   agony: number,
+# }
+def one_trip(date0, from0, to0):
+    payload = {
+        'date0': date0,
+        'from0': from0,
+        'to0': to0,
+    }
 
-results = r.json()
-routings = results['routings']
-legs = results['legs']
-itins = results['itins'].values()
+    try:
+        r = requests.post(first_url, headers=headers, data=payload)
+    except:
+        print('failed to request', sys.exc_info()[0])
+        return
 
-for itin in itins:
-    routing_iden = itin['routing_idens'][0]
-    my_routing = routings[routing_iden]
-    leg_idens = my_routing['leg_idens']
+    response = r.json()
+    if 'errors' in response:
+        print(response['errors'])
+        return
 
-    print('price', itin['price'])
-    for leg_iden in leg_idens:
-        leg = legs[leg_iden]
-        from_code = leg['from_code']
-        to_code = leg['to_code']
-        depart = leg['depart']
-        arrive = leg['arrive']
-        flight = leg['operating_num'] or leg['marketing_num']
-        airline, flight_no = flight
-        print(leg)
+    routings = response['routings']
+    legs = response['legs']
+    itins = response['itins'].values()
 
-    break
+    # construct my results from these itin objects
+    results = []
+
+    for itin in itins:
+        routing_iden = itin['routing_idens'][0]
+        my_routing = routings[routing_iden]
+        leg_idens = my_routing['leg_idens']
+        my_legs = [legs[leg_iden] for leg_iden in leg_idens]
+
+        layovers = []
+        airlines = []
+
+        # calculate layovers and airlines
+        for i, leg in enumerate(my_legs):
+            from_code = leg['from_code']
+            to_code = leg['to_code']
+
+            if i > 0:
+                # not the first one
+                prev_leg = my_legs[i - 1]
+                layovers.append({
+                    'airport': from_code,
+                    'layover': leg['depart'] - prev_leg['arrive'],
+                })
+
+            flight = leg['operating_num'] or leg['marketing_num']
+            airline, flight_no = flight
+            if airline not in airlines:
+                airlines.append(airline)
+
+        results.append({
+            'layovers': layovers,
+            'airlines': airlines,
+            'time': my_legs[-1]['arrive'] - my_legs[0]['depart'],
+            'depart': my_legs[0]['depart_iso'],
+            'arrive': my_legs[-1]['arrive_iso'],
+            'price': itin['price'],
+            'agony': itin['agony'],
+        })
+
+    # filter to <= min price * 2.5 and <= min time * 2.5
+    min_price = min(results, key=lambda result: result['price'])['price']
+    price_limit = min_price * 2.5
+    price_limit = max(price_limit, 200)
+
+    min_time = min(results, key=lambda result: result['time'])['time']
+    time_limit = min_time * 2.5
+    time_limit = max(time_limit, 3600)
+
+    filtered_results = [result for result in results if result['price'] <= price_limit and result['time'] <= time_limit]
+
+    return filtered_results
+
+pprint(one_trip('11/22/2018', 'LHR', 'JFK'))
