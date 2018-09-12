c0e60ef3610b7a8a2abcd06c31bcbda33b29ddf2
Max Friedman <maxcfriedman@gmail.com>
2018-09-11 19:55:45 -0700

    dedupe flight lists, time is hours instead of minutes

--- flighto.py
+++ flighto.py
@@ -1,7 +1,11 @@
-import requests
 from pprint import pprint
+import math
+import requests
+import sys
 
-first_url = 'https://www.hipmunk.com/api/flights/v3/load_search_first'
+stopovers = ['LHR', 'LGW', 'BOS', 'JFK']
+
+first_url = 'http://www.hipmunk.com/api/flights/v3/load_search_first'
 
 headers = {
     'accept':'application/json, text/javascript, */*; q=0.01',
@@ -18,12 +22,14 @@ headers = {
     'x-requested-with':'XMLHttpRequest',
 }
 
-stopovers = ['LHR', 'LGW', 'BOS', 'JFK']
+# approximate to 2 decimals
+def hours(minutes):
+  return math.floor(minutes / 36) / 100
 
-# filtered trips where price or time is > 2.5 the minimum
+# filtered trips where price and time is <= 2.5 the minimum
 # {
-#   layovers: [{airport: "CODE", layover: seconds}],
-#   time: seconds,
+#   layovers: [{airport: "CODE", layover: hours}],
+#   time: hours,
 #   depart: iso in local time,
 #   arrive: iso in local time,
 #   price: USD,
@@ -31,78 +37,93 @@ stopovers = ['LHR', 'LGW', 'BOS', 'JFK']
 #   agony: number,
 # }
 def one_trip(date0, from0, to0):
-    payload = {
-        'date0': date0,
-        'from0': from0,
-        'to0': to0,
-    }
+  payload = {
+      'date0': date0,
+      'from0': from0,
+      'to0': to0,
+  }
 
-    try:
-        r = requests.post(first_url, headers=headers, data=payload)
-    except:
-        print('failed to request', sys.exc_info()[0])
-        return
+  try:
+    r = requests.post(first_url, headers=headers, data=payload)
+  except:
+    print('failed to request', sys.exc_info()[0])
+    return
 
-    response = r.json()
-    if 'errors' in response:
-        print(response['errors'])
-        return
+  response = r.json()
+  if 'errors' in response:
+    print(response['errors'])
+    return
 
-    routings = response['routings']
-    legs = response['legs']
-    itins = response['itins'].values()
+  routings = response['routings']
+  legs = response['legs']
+  itins = response['itins'].values()
 
-    # construct my results from these itin objects
-    results = []
+  # construct my results from these itin objects
+  results = []
 
-    for itin in itins:
-        routing_iden = itin['routing_idens'][0]
-        my_routing = routings[routing_iden]
-        leg_idens = my_routing['leg_idens']
-        my_legs = [legs[leg_iden] for leg_iden in leg_idens]
+  for itin in itins:
+    routing_iden = itin['routing_idens'][0]
+    my_routing = routings[routing_iden]
+    leg_idens = my_routing['leg_idens']
+    my_legs = [legs[leg_iden] for leg_iden in leg_idens]
 
-        layovers = []
-        airlines = []
+    layovers = []
+    airlines = []
+    flights = []
 
-        # calculate layovers and airlines
-        for i, leg in enumerate(my_legs):
-            from_code = leg['from_code']
-            to_code = leg['to_code']
+    # calculate layovers and airlines
+    for i, leg in enumerate(my_legs):
+      from_code = leg['from_code']
+      to_code = leg['to_code']
 
-            if i > 0:
-                # not the first one
-                prev_leg = my_legs[i - 1]
-                layovers.append({
-                    'airport': from_code,
-                    'layover': leg['depart'] - prev_leg['arrive'],
-                })
-
-            flight = leg['operating_num'] or leg['marketing_num']
-            airline, flight_no = flight
-            if airline not in airlines:
-                airlines.append(airline)
-
-        results.append({
-            'layovers': layovers,
-            'airlines': airlines,
-            'time': my_legs[-1]['arrive'] - my_legs[0]['depart'],
-            'depart': my_legs[0]['depart_iso'],
-            'arrive': my_legs[-1]['arrive_iso'],
-            'price': itin['price'],
-            'agony': itin['agony'],
+      if i > 0:
+        # not the first one
+        prev_leg = my_legs[i - 1]
+        layovers.append({
+          'airport': from_code,
+          'layover': hours(leg['depart'] - prev_leg['arrive']),
         })
 
-    # filter to <= min price * 2.5 and <= min time * 2.5
-    min_price = min(results, key=lambda result: result['price'])['price']
-    price_limit = min_price * 2.5
-    price_limit = max(price_limit, 200)
+      flight = leg['operating_num'] or leg['marketing_num']
+      airline, flight_no = flight
+      if airline not in airlines:
+        airlines.append(airline)
+      flights.append(airline + str(flight_no))
 
-    min_time = min(results, key=lambda result: result['time'])['time']
-    time_limit = min_time * 2.5
-    time_limit = max(time_limit, 3600)
+    results.append({
+      'agony': itin['agony'],
+      'airlines': airlines,
+      'arrive': my_legs[-1]['arrive_iso'],
+      'depart': my_legs[0]['depart_iso'],
+      'flights': flights,
+      'layovers': layovers,
+      'price': itin['price'],
+      'time': hours(my_legs[-1]['arrive'] - my_legs[0]['depart']),
+    })
 
-    filtered_results = [result for result in results if result['price'] <= price_limit and result['time'] <= time_limit]
+  # filter to <= min price * 2.5 and <= min time * 2.5
+  min_price = min(results, key=lambda result: result['price'])['price']
+  price_limit = min_price * 2.5
+  price_limit = max(price_limit, 200)
 
-    return filtered_results
+  min_time = min(results, key=lambda result: result['time'])['time']
+  time_limit = min_time * 2.5
+  time_limit = max(time_limit, 1)
 
-pprint(one_trip('11/22/2018', 'LHR', 'JFK'))
+  filtered_results = [result for result in results if result['price'] <= price_limit and result['time'] <= time_limit]
+  sorted_results = sorted(filtered_results, key=lambda result: result['agony'])
+
+  # de-dupe on the list of flights
+  flight_lists = set()
+  deduped_results = []
+  for result in sorted_results:
+    flight_tuple = tuple(result['flights'])
+    if flight_tuple in flight_lists:
+      continue
+    flight_lists.add(flight_tuple)
+    deduped_results.append(result)
+
+  return deduped_results
+
+
+pprint(one_trip('11/21/2018', 'BCN', 'MDT'))
